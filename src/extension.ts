0fa4bb7ec7d8b75319291f4bf6b63720dc25355d
Amit Dhamu <dev@amitd.co>
2020-05-28 19:49:08 +0100

    Refactored to get two commands working

--- src/extension.ts
+++ src/extension.ts
@@ -3,33 +3,72 @@ import * as child from 'child_process';
 import * as findUp from 'find-up';
 import * as path from 'path';
 
+const getCurrentRepository = async (file: string): Promise<string | null> => {
+  const repository = await findUp('.git', {
+    cwd: path.dirname(file),
+    type: 'directory',
+  });
+
+  return path.dirname(repository ?? '');
+};
+
+const openSublimeMerge = (args: string[], repository: string) => {
+  console.log(args);
+
+  child.execFile('smerge', args, {
+    cwd: repository,
+  });
+};
+
+const getFileDetails = async (editor: vscode.TextEditor) => {
+  const repository = await getCurrentRepository(editor.document.uri.path);
+
+  return {
+    path: editor.document.uri.path.replace(`${repository}/`, ''),
+    currentLineNumber: editor.selection.active.line + 1,
+    repository: repository ?? '',
+  };
+};
+
+const viewFileHistory = async () => {
+  if (vscode.window.activeTextEditor) {
+    const { path, repository } = await getFileDetails(
+      vscode.window.activeTextEditor
+    );
+
+    openSublimeMerge(['search', `file:"${path}"`], repository);
+  }
+};
+
+const viewLineHistory = async () => {
+  if (vscode.window.activeTextEditor) {
+    const { path, repository, currentLineNumber } = await getFileDetails(
+      vscode.window.activeTextEditor
+    );
+
+    openSublimeMerge(
+      [
+        'search',
+        `file:"${path}" line:${currentLineNumber}-${currentLineNumber}`,
+      ],
+      repository
+    );
+  }
+};
+
 export function activate(context: vscode.ExtensionContext) {
   const viewFileHistoryCommand = vscode.commands.registerCommand(
     'history-in-sublime-merge.viewFileHistory',
     viewFileHistory
   );
 
+  const viewLineHistoryCommand = vscode.commands.registerCommand(
+    'history-in-sublime-merge.viewLineHistory',
+    viewLineHistory
+  );
+
   context.subscriptions.push(viewFileHistoryCommand);
+  context.subscriptions.push(viewLineHistoryCommand);
 }
 
 export function deactivate() {}
-
-const viewFileHistory = async () => {
-  if (vscode.window.activeTextEditor) {
-    let filePath = vscode.window.activeTextEditor.document.uri.path;
-    const findRepo = await findUp('.git', {
-      cwd: path.dirname(filePath),
-      type: 'directory',
-    });
-
-    if (!findRepo) {
-      return;
-    }
-
-    const repoLocation = path.dirname(findRepo);
-    filePath = filePath.replace(`${repoLocation}/`, '');
-    child.execFile(`smerge`, ['search', `file:"${filePath}"`], {
-      cwd: repoLocation,
-    });
-  }
-};
